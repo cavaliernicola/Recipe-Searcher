b525fa866fe2609547a4ba2952ae1919ef614d22
cavaliernicola <cavaliernicola@gmail.com>
2023-05-14 19:32:30 +0200

    handle ingredients without image

--- src/components/Ingredient/IngredientItem/IngredientItem.jsx
+++ src/components/Ingredient/IngredientItem/IngredientItem.jsx
@@ -1,8 +1,10 @@
+import placeholder from "../../../assets/img/no-ingredient-placeholder.png";
+
 export default function IngredientItem({ original, image, name }) {
   return (
     <div className="ingredient-container">
       <div className="ingredient-card">
-        <img className="card-content" src={`https://spoonacular.com/cdn/ingredients_100x100/${image}`} alt={name} />
+        <img className="card-content" src={ image ? `https://spoonacular.com/cdn/ingredients_100x100/${image}` : placeholder} alt={name} />
         <p className="title">{original}</p>
       </div>
     </div>
